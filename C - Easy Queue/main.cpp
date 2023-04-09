e8c018403ba002a326307033d35805d35ac955b6
Omar R <oradwan037@gmail.com>
2023-04-09 22:20:16 +0200

    Add files via upload

---
+++ C - Easy Queue/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "queue"
+using namespace std;
+
+int main() {
+    int operations,action,number;
+    cin.tie(0), cout.tie(0);
+    queue <int> myQueue;
+    cin>>operations;
+    while (operations--) {
+            cin>>action;
+            if (action == 1) {
+                cin >> number;
+                myQueue.push(number);
+            } else if (action == 2) {
+                if (!myQueue.empty())myQueue.pop();
+            } else {
+                if (!myQueue.empty())cout<<myQueue.front()<<'\n';
+                else cout<<"Empty!"<<'\n';
+            }
+    }
+    return 0;
+}
